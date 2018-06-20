8e0c2f8d66c2c3ad3a62af5b15df246d1b668e05
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2018-06-20 11:19:46 +0200

    new beta license has a million minutes

--- test/with_license_test.go
+++ test/with_license_test.go
@@ -71,8 +71,8 @@ func getLicenseTimeTotal(licensesMetrics string) int {
 func TestThatMoreThanFiveSessionsWorkWithALicense(t *testing.T) {
 
 	var nbrIterations = 10
-	var costPerSession = 6      // Each session cost x nbr of analyzer minutes
-	var totalTimeLicense = 1000 // Total number of analyzer minutes specified in license
+	var costPerSession = 6         // Each session cost x nbr of analyzer minutes
+	var totalTimeLicense = 1000000 // Total number of analyzer minutes specified in license
 
 	for i := 0; i < nbrIterations; i++ {
 		message, err := ConnectToEngineAndReturnOnConnectedEventMessage(ctx, i, headers)
