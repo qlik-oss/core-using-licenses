05b378de7f7a14b2b98d1c4ce82ac0239b491f26
Jan Miderbäck <jan.miderback@gmail.com>
2018-08-13 15:23:00 +0200

    Verify SESSION_ERROR_LIMIT_EXCEEDED push message (#23)

--- test/no_license_test.go
+++ test/no_license_test.go
@@ -1,10 +1,8 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/qlik-oss/enigma-go"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,11 +18,8 @@ func TestThatFiveButNotMoreConcurentSessionsWork(t *testing.T) {
 	}
 
 	//One more connection than allowed, this should not be allowed
-	headers.Set("X-Qlik-Session", fmt.Sprintf("%d", numberOfAllowedConnectionsWithNoLicenseService+1))
-	global, err := enigma.Dialer{}.Dial(ctx, fmt.Sprintf("ws://%s:19076/app/engineData/", host), headers)
-	assert.Nil(t, err, "Failed to open websocket to Qlik Analytics Engine")
-	//try to create a session app to see that the WS has been closed (In the future engine should send the SESSION_ERROR_NO_LICENSE event here.)
-	_, err = global.CreateSessionApp(ctx)
-	assert.NotNil(t, err, "This session should not be allowed to be open")
+	message, err := ConnectToEngineAndReturnOnConnectedEventMessage(ctx, numberOfAllowedConnectionsWithNoLicenseService+1, headers)
+	assert.Equal(t, "SESSION_ERROR_LIMIT_EXCEEDED", message)
+	assert.Nil(t, err, "This session should not be allowed to be open")
 
 }
